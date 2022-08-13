016eae4c4e470e0ca5210a760b7d56cbac576af0
jjeongwwoo <sjw050607@naver.com>
2022-08-13 18:06:05 +0900

    8월 13일 puppeteer을 사용한 크롤링 테스트.

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
+const { request, response } = require('express');
 const express = require('express');
+const puppeteer = require('puppeteer');
 const app = express();
 
 app.use(express.json());
@@ -22,6 +24,32 @@ app.post('/school-meal', (request, response)=>{
         
         response.json(chatbotResponse)
     }
+});
+
+app.post('/school-meal/daily', (request, response)=>{
+    const utterance = request.body.userRequest.utterance;
+
+    //todo : 월/일 형식이 맞는지 확인
+
+    //todo : month = 월, date = 일
+
+    //todo : '점심' 버튼 click
+
+
+});
+
+app.get('/crawling/test', async(request, response)=>{
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://sunrint.sen.hs.kr/65129/subMenu.do');
+
+    const table = await page.$('table > tbody')
+    console.log(table)
+
+    //await page.click('table > tbody');
+
+
+    await browser.close();
 });
 
 app.listen(3000, ()=> console.log("node on 3000"));
